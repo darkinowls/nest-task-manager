ee10968541a55d4ac9cc3e0d4ab15089d9a7fd61
Denys Chernousov <blackInOwls@gmail.com>
2024-05-03 13:21:49 +0300

    include fixed migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,39 +1,39 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { TasksModule } from "./tasks/tasks.module";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { ConfigModule } from "@nestjs/config";
-import { UserModule } from "./user/user.module";
-import { JwtModule } from "@nestjs/jwt";
-import databaseConfig from '@typeorm.config';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(
-      {
-        isGlobal: true
-      }
-    ),
-    TypeOrmModule.forRootAsync(
-      {
-        useFactory: () => ({
-          type: "postgres",
-          username: process.env.POSTGRES_USER,
-          password: process.env.POSTGRES_PASSWORD,
-          host: process.env.POSTGRES_HOST,
-          database: process.env.POSTGRES_DB,
-          port: parseInt(process.env.POSTGRES_PORT || "5432"),
-          autoLoadEntities: true,
-        })
-      }
-    ),
+	imports: [
+		ConfigModule.forRoot(
+			{
+				isGlobal: true
+			}
+		),
+		TypeOrmModule.forRootAsync(
+			{
+				useFactory: () => ({
+					type: 'postgres',
+					username: process.env.POSTGRES_USER,
+					password: process.env.POSTGRES_PASSWORD,
+					host: process.env.POSTGRES_HOST,
+					database: process.env.POSTGRES_DB,
+					port: parseInt(process.env.POSTGRES_PORT || '5432'),
+					autoLoadEntities: true
+				})
+			}
+		),
 
 
-    UserModule,
-    TasksModule
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+		UserModule,
+		TasksModule
+	],
+	controllers: [AppController],
+	providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+}
